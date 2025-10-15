597a8590cc295ea2dacb270ae4359e5c5060333f
ruihong04 <robin.sh@qq.com>
2025-10-15 23:49:16 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ setup(
     name="chamfer-distance",
     version="0.1.0",
     description="Chamfer Distance PyTorch extension (minimal packaging)",
-    author="",
     packages=find_packages(include=["chamfer_distance", "chamfer_distance.*"]),
+    include_package_data=True,
+    package_data={"chamfer_distance": ["*.cpp", "*.cu"]},
     python_requires=">=3.8",
 )
